c8244077c26979c60919f6d3aa67153f3810b8cb
yucarl77 <yucarl77@foxmail.com>
2022-08-29 22:41:36 +0800

    chore: better build generate d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,26 @@
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 import del from "rollup-plugin-delete";
+import dts from "rollup-plugin-dts";
 
 const outputPath = "./dist";
 
-export default {
-  input: "./src/index.ts",
-  output: {
-    file: outputPath + "/index.js",
-    name: "OmpWrapper",
-    format: "umd",
-  },
+export default [
+  {
+    input: "./src/index.ts",
+    output: {
+      file: outputPath + "/bundle.js",
+      format: "cjs",
+    },
 
-  plugins: [del({ targets: outputPath + "/*" }), typescript(), terser()],
-};
+    plugins: [del({ targets: outputPath + "/*" }), typescript(), terser()],
+  },
+  {
+    input: "./src/index.ts",
+    output: {
+      file: outputPath + "/bundle.d.ts",
+      format: "cjs",
+    },
+    plugins: [dts()],
+  },
+];
